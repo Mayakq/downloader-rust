e19c585ae136a46d5faf72ac06cfe8b6e59cba25
Mayak <gods@mast.com>
2023-12-24 18:27:25 +0400

    async version

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 pub mod lib {
-
     use core::panic;
-    use std::fs::File;
+    use std::{fs::File, ops::Deref, sync::Arc};
 
     use futures::StreamExt;
+    use tokio::sync::Mutex;
     use url::Url;
+
     #[derive(Debug)]
     pub struct Download {
         pub url: Vec<Url>,
         pub extension: String,
         pub directory_for_save: Directory,
     }
+
     #[derive(Debug)]
     pub enum Directory {
         /// Default is current directory ./
@@ -27,30 +29,34 @@ pub mod lib {
             })
         }
         pub async fn download(&self) {
-            let client = reqwest::Client::new();
-            for url in &self.url {
-                let response = client.get(url.to_string()).send().await.unwrap();
-                let length = response.content_length().unwrap(); // todo(PROGRESS BAR)
-                let mut stream = response.bytes_stream();
-                let mut file = File::create("./data.jpg").unwrap();
-                while let Some(chunk) = stream.next().await {
-                    let item = chunk.unwrap();
-                    std::io::Write::write_all(&mut file, &item).unwrap();
-                }
+            let mut counter = 0;
+            let mut threads = Vec::new();
+            for url in self.url.clone() {
+                let thread = tokio::spawn(async move {
+                    write_bytes(url, counter).await;
+                })
+                .await;
+                counter +=1;
+                threads.push(thread);
+            }
+            for thread in threads {
+                thread.unwrap();
             }
         }
     }
-    pub async fn read_file(path: String) -> Option<Vec<u8>> {
+
+    pub async fn read_file(path: String) -> Option<String> {
         let lines = tokio::fs::read(path).await;
         match lines {
             Ok(result) => {
-                return Some(result);
+                return Some(String::from_utf8(result).unwrap());
             }
             Err(err) => {
                 panic!("{}", err)
             }
         }
     }
+
     pub async fn get_url(array_str: String) -> Vec<Url> {
         let mut vec = Vec::with_capacity(10);
         for elem in array_str.split("\n") {
@@ -65,5 +71,18 @@ pub mod lib {
             panic!("Error parse url from file. Count parsed url = 0");
         }
         vec
+    }
+
+    pub async fn write_bytes(url: Url, counter: u32) {
+        let client = reqwest::Client::new();
+        let response = client.get(url.to_string()).send().await.unwrap();
+        let _length = response.content_length().unwrap(); // todo(PROGRESS BAR)
+        let mut stream = response.bytes_stream();
+        let path = format!("./{}.{}", counter, "png");
+        let mut file = File::create(path).unwrap();
+        while let Some(chunk) = stream.next().await {
+            let item = chunk.unwrap();
+            std::io::Write::write_all(&mut file, &item).unwrap();
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
-use dwmanager::lib::{Directory, Download, read_file};
-use url::Url;
+use dwmanager::lib::{read_file, Directory, Download};
+
 #[tokio::main]
 async fn main() {
     start().await;
 
 }
-async fn start(){
-    let url = Url::parse(
-        "https://yastat.net/s3/rasp/s/morda-front/_/images/hotelBanner/bannerDesktop.jpg",
-    );
-    let extension = ".png".to_string();
+async fn start() {
+    let extension = "png".to_string();
     let directory = Directory::Default;
     let urls = read_file("./urls.txt".to_string()).await;
-    
-    match url {
-        Err(err) => {
-            panic!("{}", err.to_string())
-        }
-        Ok(url) => {
-            let manager = Download::new(url, extension, directory).unwrap();
-
-        }
-    }
+    let url = dwmanager::lib::get_url(urls.unwrap()).await;
+    let manager = Download::new(url, extension, directory).unwrap();
+    manager.download().await;
 }
+
